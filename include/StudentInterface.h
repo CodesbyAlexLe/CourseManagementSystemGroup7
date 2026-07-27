6ee1fbc554474a4028f6eb29cea868bb46bc4f59
Alex Le <alexle16@hotmail.com>
2026-07-26 22:54:49 -0400

    Add Cart inclusion and update StudentInterface class

--- include/StudentInterface.h
+++ include/StudentInterface.h
@@ -1,3 +1,13 @@
 #pragma once
 #include <vector>
 #include "department.h"
+#include "cart.h"
+
+class StudentInterface {
+public:
+    StudentInterface(std::vector<Department>& departments);
+    void run();
+
+private:
+    std::vector<Department>& departments;
+    Cart cart;
