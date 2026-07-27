0da5c406146a4abaa190742bafea24eb982efa6a
Alex Le <alexle16@hotmail.com>
2026-07-26 22:47:04 -0400

    Add AdminInterface class declaration

--- include/AdminInterface.h
+++ include/AdminInterface.h
@@ -1,1 +1,19 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "department.h"
 
+class AdminInterface {
+public:
+    AdmionInterface(std::vector<Department>& departments, const std::string& dataFilePath);
+    void run();
+
+private:
+    std::vector<Department>& departments;
+    std::string dataFilePath;
+
+    void listDepartments();
+    void addDepartment();
+    void addCourseToDepartment();
+    void saveChanges();
+};
