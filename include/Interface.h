f49edafc0ab2ef9137aefed73f3455c7c5e3a3be
Alex Le <alexle16@hotmail.com>
2026-07-26 22:23:12 -0400

    Implement Interface class for user interaction
    Added Interface class with methods for admin and student menus.

--- include/Interface.h
+++ include/Interface.h
@@ -1,1 +1,32 @@
+#include <string>
+#include <vector>
+#include "DataStore.h"
+#include "CartItem.h"
 
+class Interface {
+public:
+    Interface(const std::string& dataFile);
+    void run();
+
+private:
+    DataStore store;
+    std::vector<CartItem> cart;
+    std::string dataFilePath;
+
+    //Initial Menu
+    void showInitialMenu();
+
+    //Admin Interface
+    void adminMenu();
+    void listDepartments();
+    void addDepartment();
+    void addCourseToDepartment();
+    void saveChanges();
+
+    // Student Interface
+    void studentMenu();
+    void showCart();
+    void checkout();
+    void browseDepartments();
+    void listCoursesInDepartment(Department& dept);
+    void addCourseToCart(Department& dept);
