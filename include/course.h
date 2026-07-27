e61a7a949f1b21791afcc7110ca4f5dd235b2a81
Alex Le <alexle16@hotmail.com>
2026-07-26 22:50:21 -0400

    Define Course class with attributes and methods
    Added Course class with member variables and constructors.

--- include/course.h
+++ include/course.h
@@ -1,1 +1,16 @@
+#pragma once
+#include <string>
 
+class Course {
+public:
+    std::string courseNumber;
+    std::string courseName;
+    std::string schedule;
+    double price;
+
+    Course();
+    Course(const std::string& number, const std::string& name, const std::string& sched, double p);
+
+    // Prints "Name (Number) | Schedule | $Price"
+    void display() const;
+};
