9c485bae3d30f05400af13ffffc31a2888b7ca20
Alex Le <alexle16@hotmail.com>
2026-07-26 22:52:14 -0400

    Add Cart class with course management methods

--- include/cart.h
+++ include/cart.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <vector>
+#include "course.h"
 
+class Cart {
+public:
+    void addCourse(const Course& course);
+    bool isEmpty() const;
+    void clear();
+    void display() const;
+    const std::vector<Course>& getItems() const;
+
+private:
+    std::vector<Course> items;
+};
