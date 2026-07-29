43cbac99836391c0fa79eee74c5ae02b75d0966c
Alex Le <alexle16@hotmail.com>
2026-07-28 23:07:08 -0400

    Implement AdminInterface constructor and run method
    Added constructor and run method for AdminInterface.

--- src/AdminInterface.cpp
+++ src/AdminInterface.cpp
@@ -1,1 +1,11 @@
+#include "AdminInterface.h"
+#include "Interface.h"
+#include <iostream>
 
+AdminInterface::AdminInterface(std::vector<Department>& depts, const std::string% filePath)
+    : departments(depts), dataFilePath(filePath) {
+}
+
+void AdminInterface::run() {
+    while (true) {
+        std::cout << "\n----- Admin Main Menu -----\n";
